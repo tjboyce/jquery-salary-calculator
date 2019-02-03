51f4154d176ea33a3f17d38349f0f58902e522b1
Todd Boyce Jr <tjboyce@me.com>
2019-02-03 14:03:45 -0600

    button, inputs, and clear inputs done

--- client.js
+++ client.js
@@ -1,7 +1,20 @@
 console.log('js');
 
-$(document).ready(onready);
+$(document).ready(onReady);
 
 function onReady (){
+    $('#submitButton').on('click', addEmployee);
+        console.log('in button click');
     
+}
+
+function addEmployee (){
+    console.log('in add employee');
+    let firstName = $('#firstNameIn').val();
+    let lastName = $('#lastNameIn').val();
+    let idNumber = $('#idIn').val();
+    let title = $('#titleIn').val();
+    let annualSalary = $('#annualSalaryIn').val();
+    $('#newRow').append(`<tr><td>`+ firstName + `</td> <td>`+ lastName + `</td><td>` + idNumber + `</td><td>`+ title + `</td><td>`+ annualSalary +`</td></tr>`);
+    $('.inputClass').val('');
 }
